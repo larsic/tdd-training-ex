c7d21f35943d289531ac2647c50bebe2d27f1a96
LVKBM43 <larsvk@gmail.com>
2019-06-03 10:42:54 +0200

    here it is

--- business/src/main/java/be/gfi/academy/tdd/exercise/StringCalculator.java
+++ business/src/main/java/be/gfi/academy/tdd/exercise/StringCalculator.java
@@ -1,5 +1,7 @@
 package be.gfi.academy.tdd.exercise;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
 /**
  * Created by BPCO on 17/06/2018.
  * Class to perform calculations with strings
@@ -8,12 +10,65 @@ public class StringCalculator {
     /**
      * Method to calculate the sum of numbers in a given string
      */
-    public static final void add(){
-        //String[] numbersArray = numbers.split(",");
+    public static final int add(String numbers, String delimiter){
 
-        //Test arguments are correct
-        //If more than 3 numbers than runtime exception
-        //else If not a valid number, than runtime exception
+        if(numbers.equals("")) {
+            return 0;
+        } else{
+            numbers = numbers.replace("n", delimiter);
+        }
+
+        if(delimiter.contains("[")) delimiter = createDelimiter(delimiter);
+
+
+        String[] numbersArray = numbers.split(delimiter);
+
+        int[] parsedNumbers = getArrayOfParsedNumbers(numbersArray);
+
+
+        checkForNegativeNumbers(parsedNumbers);
+
+        return (int)  Arrays.stream(parsedNumbers)
+                            .filter(i -> i <= 1000)
+                            .sum();
+    }
+
+
+    public static String createDelimiter(String delimiter) {
+
+        delimiter = delimiter.replaceAll("\\]", "");
+        delimiter = delimiter.replaceAll("\\[", "|");
+        delimiter = "[" + delimiter.substring(1) + "]+";
+
+        return delimiter;
+    }
+
+
+    private static int[] getArrayOfParsedNumbers(String[] numbersArray) {
+        return Arrays.stream(numbersArray)
+                                    .mapToInt(StringCalculator::parseOrThrow)
+                                    .toArray();
+    }
+
+
+    public static int parseOrThrow(String number) {
+
+        try {
+            return Integer.parseInt(number);
+        } catch (Exception e){
+            throw new RuntimeException();
+        }
+    }
+
+
+    public static void checkForNegativeNumbers(int [] numbersArray) {
+
+        String negativeNumbers = Arrays.stream(numbersArray)
+                                       .filter(i -> i < 0)
+                                       .mapToObj(s -> String.valueOf(s))
+                                       .collect(Collectors.joining(","));
+
+        if(negativeNumbers.length() > 0) throw new RuntimeException(negativeNumbers);
     }
 }
 
--- business/src/test/java/be/gfi/academy/tdd/exercise/StringCalculatorTest.java
+++ business/src/test/java/be/gfi/academy/tdd/exercise/StringCalculatorTest.java
@@ -1,6 +1,5 @@
 package be.gfi.academy.tdd.exercise;
 
-import be.gfi.academy.tdd.exercise.StringCalculator;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,16 +8,79 @@ import org.junit.Test;
  */
 public class StringCalculatorTest {
 
-    @Test(expected = RuntimeException.class)
-    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
-        StringCalculator.add("1,2,3");
-    }
-
+    @Test
     public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
+
+        StringCalculator.add("1,2", ",");
         Assert.assertTrue(true);
     }
 
+    @Test(expected = RuntimeException.class)
     public final void whenNonNumberIsUsedThenExceptionIsThrown() {
+        StringCalculator.add("1,a", ",");
     }
+
+    @Test
+    public final void whenStringISEmptyReturn0() {
+        Assert.assertEquals(0, StringCalculator.add("", ","));
+    }
+
+    @Test
+    public final void checkSumWithOneNumber() {
+        Assert.assertEquals(5, StringCalculator.add("5", "%"));
+    }
+
+    @Test
+    public final void checkSumWithTwoNumbers() {
+        Assert.assertEquals(17, StringCalculator.add("5,12", ","));
+    }
+
+    @Test
+    public final void checkSumWithLotsOfNumbers() {
+        Assert.assertEquals(110, StringCalculator.add("5,12,8,5,70,2,3,4,1", ","));
+    }
+
+    @Test
+    public final void chooseDelimiter() {
+        Assert.assertEquals(1022, StringCalculator.add("345///566///111", "///"));
+    }
+
+    @Test
+    public final void handleNewLines() {
+        Assert.assertEquals(21, StringCalculator.add("1/2n3/4n5n6", "/"));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public final void negativeNumbersThrowAnException() {
+        StringCalculator.add("-1", ",");
+    }
+
+    @Test
+    public final void negativeNumbersThrowAnExceptionWithSpecificMessage() {
+        try {
+            StringCalculator.add("-1,2,-3", ",");
+        } catch (Exception e){
+            Assert.assertEquals(e.getMessage(),"-1,-3");
+        }
+    }
+
+    @Test
+    public final void numbersBiggerThan1000ShouldBeIgnored() {
+        ;
+        Assert.assertEquals(2000, StringCalculator.add("1000,1001,999,2000,1", ","));
+    }
+
+    @Test
+    public final void multipleDelimitersTested() {
+
+        Assert.assertEquals(100, StringCalculator.add("20,30%40,%10", "[,][%]"));
+    }
+
+
+//    @Test(expected = RuntimeException.class)
+//    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
+//
+//        StringCalculator.add("1,2,3");
+//    }
 
 }
